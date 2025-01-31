52063f5431f617dc38141df9471eccf51736afa1
JordanDavidAx <jdavid@axome.com>
2025-01-31 17:16:29 +0100

    :fire: remove error and warning

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -4,28 +4,31 @@ import './gallery.scss'
 import useFetch from "../../hooks/useFetch.jsx";
 
 const Gallery = () => {
+  // [getter, setter] = useState(initialValue)
   const [url, setUrl] = useState("/data/logements.json");
   const { data: logements, isPending, error } = useFetch(url);
+  // on aura pu simplement mettre const { data: logements, isPending, error } = useFetch("/data/logements.json");
+  // mais on pense a plus tard ou on aura besoin de changer l'url pour des filtrages par exemple
 
   return (
-    <section className="gallery__container">
-      {error && <p>{error}</p>}
+      <section className="gallery__container">
+        {error && <p>{error}</p>}
 
-      {isPending && <p>Chargement...</p>}
+        {isPending && <p>Chargement...</p>}
 
-      {logements?.length > 0 ? (
-        <ul>
-          {logements.map((logement) => (
-            <GalleryCard
-              key={logement.id}
-              {...logement}
-            />
-          ))}
-        </ul>
-      ) : (
-        <h2>Aucun logement pour le moment.</h2>
-      )}
-    </section>
+        {logements?.length > 0 ? (
+            <ul>
+              {logements.map((logement) => (
+                  <GalleryCard
+                      key={logement.id}
+                      {...logement}
+                  />
+              ))}
+            </ul>
+        ) : (
+            <h2>Aucun logement pour le moment.</h2>
+        )}
+      </section>
   );
 };
 
--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,25 +2,26 @@ import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
-  const [isPending, setIsPending] = useState(false);
+  const [isPending, setIsPending] = useState(true);
   const [error, setError] = useState(null);
 
+  // Utilisation du useEffect pour que ça séxecute une fois que le composant est monté et à chaque fois que l'url change
+  // Sans useEffect, le code serait exécuté à chaque rendu du composant
   useEffect(() => {
-    const fetchData = async () => {
-      setIsPending(true);
-      try {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error(response.statusText);
-        const json = await response.json();
+    // Fetch est une fonction asynchrone qui renvoie une promesse (API native du navigateur)
+    fetch(url)
+      .then(res => {
+        if (!res.ok) throw new Error('Erreur lors de la récupération des données');
+        return res.json();
+      })
+      .then(data => {
+        setData(data);
         setIsPending(false);
-        setData(json);
-        setError(null);
-      } catch (error) {
-        setError('Oops! Il y a eu une erreur : ' + error.message);
+      })
+      .catch(err => {
+        setError(err.message);
         setIsPending(false);
-      }
-    };
-    fetchData();
+      });
   }, [url]);
 
   return { data, isPending, error };
