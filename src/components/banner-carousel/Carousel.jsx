035f0d51d00fa8bcd8de0fc370fa82ae262955b3
JordanDavidAx <jdavid@axome.com>
2025-01-31 15:39:09 +0100

    :lipstick: Correction avant passage oral

--- src/components/banner-carousel/Carousel.jsx
+++ src/components/banner-carousel/Carousel.jsx
@@ -3,45 +3,23 @@ import { useState } from "react";
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
-  const [preloadBeforeIndex, setPreloadBeforeIndex] = useState(images.length - 1)
-  const [preloadNextIndex, setPreloadNextIndex] = useState(1)
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
     const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex)
-    preloadIndex()
   }
 
   const nextSlide = () => {
     const isLastSlide = currentIndex === images.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(newIndex)
-    preloadIndex()
-  }
-
-  const preloadIndex = () => {
-    const isFirstSlide = currentIndex === 0;
-    const isLastSlide = currentIndex === images.length - 1;
-    const newIndexPrev = isFirstSlide ? images.length - 2 : currentIndex - 2
-    const newIndexNext = isLastSlide ? 1 : currentIndex + 2
-
-    setPreloadBeforeIndex(newIndexPrev)
-    setPreloadNextIndex(newIndexNext)
   }
 
   return (
     <section className='carousel__container'>
       {images &&
         <div className='carousel'>
-          {/* Trick to preload prev/next images to have a smooth animation */}
-          {images.length > 1 && (
-            <>
-              <div className='slide preload' style={{ backgroundImage: `url(${images[preloadBeforeIndex]})` }} />
-              <div className='slide preload' style={{ backgroundImage: `url(${images[preloadNextIndex]})` }} />
-            </>
-          )}
-
           {/* Slide showing */}
           <div className='slide' style={{ backgroundImage: `url(${images[currentIndex]})` }}></div>
 
@@ -58,10 +36,8 @@ const Carousel = ({ images }) => {
           )}
         </div>
       }
-
     </section>
-  )
-    ;
+  );
 };
 
 export default Carousel;
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,19 +1,16 @@
 import { Link, useRouteError } from "react-router-dom";
 import './error.scss';
 
-const Error = ({ errorMessage }) => {
+const Error = () => {
   const error = useRouteError();
 
   return (
     <section id="error-page">
       <h1 className='text-404'>404</h1>
-      {error && error}
-      {errorMessage ?
-        <p className='error__message'>{errorMessage}</p> :
-        <p className='error__message'>Oups! La page que vous demandez n'existe pas.</p>
-      }
-
-      <Link className='home__link' to={'/'}>Retourner sur la page d’accueil</Link>
+      <p className='error__message'>
+        {error?.message || "Oups! La page que vous demandez n'existe pas."}
+      </p>
+      <Link className='home__link' to={'/'}>Retourner sur la page d'accueil</Link>
     </section>
   );
 };
