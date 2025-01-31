6b2f2c05aabf857873ba11793baa78d43cdd08c2
JordanDavidAx <jdavid@axome.com>
2025-01-31 17:44:08 +0100

    :sparkles: simplification

--- src/routes/Logements.jsx
+++ src/routes/Logements.jsx
@@ -1,34 +1,25 @@
 import LogementPage from '../pages/logement/LogementPage.jsx'
 import { Navigate, useLoaderData, useNavigation } from 'react-router-dom'
 
-export const logementLoader = async (params) => {
-  const response = await fetch('/data/logements.json')
-  if (response.status === 404) return { logement: null }
-  if (!response.ok) throw new Error(response.statusText)
-  const json = await response.json()
-  const logementId = params.logementsId
-
-  const getLogementWithId = (logementsId) => {
-    return json.find(logement => logement.id === logementsId)
+export const logementLoader = async ({ logementsId }) => {
+  try {
+    const response = await fetch('/data/logements.json')
+    const logements = await response.json()
+    return {
+      logement: logements.find(logement => logement.id === logementsId) || null
+    }
+  } catch {
+    return { logement: null }
   }
-
-  if (logementId) {
-    return { logement: getLogementWithId(logementId) }
-  }
-
-  return { logement: null }
 }
 
 const Logements = () => {
   const { logement } = useLoaderData()
-  const { state } = useNavigation();
+  const { state } = useNavigation()
 
-  return (
-    <>
-      {state === 'loading' && <div>Loading...</div>}
-      {logement ? <LogementPage logement={logement} /> : <Navigate replace to='/404' />}
-    </>
-  )
+  if (state === 'loading') return <div>Loading...</div>
+
+  return logement ? <LogementPage logement={logement} /> : <Navigate replace to='/404' />
 }
 
 export default Logements
